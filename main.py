da7527dd8d34c3db722acf10bdfa639f92be7e9a
Akshat Tomar <akshat_t@mfs.iitr.ac.in>
2025-08-09 21:14:16 +0530

    value of n=initial sequence length

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import torch as nn
+import torch
 import evaluate
 from datasets import load_dataset
 from transformers import AutoTokenizer
@@ -24,52 +24,51 @@ xsum = load_dataset("xsum", split=f"test[:{config['dataset']['size']}]",trust_re
 tokenizer = AutoTokenizer.from_pretrained(config['models']['tokenizer'])
 
 
-original_summary=[]
-speculative_summary=[]
-acceptance_rate=0
-target_speed=0
-speculative_speed=0
+# original_summary=[]
+# speculative_summary=[]
+# acceptance_rate=0
+# target_speed=0
+# speculative_speed=0
+results = []
 
 for sample in tqdm(xsum):
     
 
     prompt="Generate a short Summary for this article: "+sample['document']
-    n=prompt.size()
+    n=len(prompt)
 
     target_ids,target_sp=auto_regressive(prompt)
     speculative_ids,acc_rate,speculative_sp=speculative_sampling(prompt)
 
 
-    acceptance_rate+=acc_rate
-    target_speed+=target_sp
-    speculative_speed+=speculative_sp
+    # acceptance_rate+=acc_rate
+    # target_speed+=target_sp
+    # speculative_speed+=speculative_sp
 
 
-    original_summary.append(tokenizer.decode(target_ids[0][n:], skip_special_tokens=True))
-    speculative_summary.append(tokenizer.decode(speculative_ids[0][n:], skip_special_tokens=True))
+    original_summary=tokenizer.decode(target_ids[0], skip_special_tokens=True)[n:]
+    speculative_summary=tokenizer.decode(speculative_ids[0], skip_special_tokens=True)[n:]
 
-
-
-target_speed/=config['dataset']['size']
-speculative_speed/=config['dataset']['size']
-acceptance_rate/=config['dataset']['size']
-
-print(target_speed,speculative_speed,acceptance_rate)
-
-
-results = []
-results.append({
-    "prompt": xsum['document'],
-    "reference": xsum['summary'],
+    results.append({
+    "prompt": sample,
+    "reference": sample['summary'],
     "speculative_summary": speculative_summary,
     "original_summary": original_summary,
-    "acceptance_ratio": acceptance_rate,
-    "inference_speed": target_speed,
-    "speculative_speed": speculative_speed
+    "acceptance_ratio": acc_rate,
+    "inference_speed": target_sp,
+    "speculative_speed": speculative_sp
+    })
 
-})
+
+
+# target_speed/=config['dataset']['size']
+# speculative_speed/=config['dataset']['size']
+# acceptance_rate/=config['dataset']['size']
+
+# print(target_speed,speculative_speed,acceptance_rate)
 
 
 with open("results.jsonl", "a", encoding="utf-8") as f:
     for r in results:
         f.write(json.dumps(r, ensure_ascii=False) + "\n")
+
